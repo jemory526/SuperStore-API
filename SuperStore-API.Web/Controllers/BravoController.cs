81c845c7c2124bdc04d55e54443b822c16a65a9b
Jordan Emory <jemory526@gmail.com>
2019-11-22 18:29:52 -0500

    Added simple bravo hello get call

---
+++ SuperStore-API.Web/Controllers/BravoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SuperStore_API.Web.Controllers
+{
+    [RoutePrefix("api/bravo")]
+    public class BravoController : ApiController
+    {
+        [Route("hello"), HttpGet]
+        public HttpResponseMessage Hello()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello");
+        }
+    }
+}
